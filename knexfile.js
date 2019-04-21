015a419b8b8bfa4e6f908e5fad3b260710a7ee92
Bolaji Olajide <cooproton@gmail.com>
2019-04-21 18:33:34 +0100

    allow foreign key assoc on sqlite

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,37 @@
 // Update with your config settings.
 
+const baseCfg = {
+  debug: false,
+  pool: { min: 2, max: 10 },
+  migrations: { tableName: 'knex_migrations' },
+  seeds: { directory: './seeds' },
+  debug: false
+};
+
 module.exports = {
 
   development: {
+    ...baseCfg,
     client: 'sqlite3',
     connection: { filename: './movie.sqlite' },
-    migrations: { tableName: 'knex_migrations' },
-    seeds: { directory: './seeds' },
-    debug: false,
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    pool: {
+      // adding this because sqlite doesn't fully support foregn keys
+      // so when you delete a movie it doesn't delete it's references in the
+      // many-to-many table.
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys=ON', cb)
+    }
   },
 
   production: {
+    ...baseCfg,
     client: 'pg',
     connection: {
-      host:     'localhost',
-      user:     'username',
+      host: 'localhost',
+      user: 'username',
       password: 'password',
       database: 'movie'
-    },
-    pool: { min: 2, max: 10 },
-    migrations: { tableName: 'knex_migrations' },
-    seeds: { directory: './seeds' },
-    debug: false
+    }
   }
 
 };
